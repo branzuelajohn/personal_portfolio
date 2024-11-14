bde27a9ff268b12c39e96306d1374dd68ac6c757
branzuelajohn <branzuelajohn@gmail.com>
2024-11-14 09:19:29 +0800

    update skill logos

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -22,28 +22,37 @@ import {
   SiNextdotjs,
   SiTailwindcss,
   SiMysql,
+  SiAwsamplify,
+  SiAngular,
+  SiNodedotjs,
+  SiExpress,
+  SiDocker,
+  SiKubernetes,
+  SiCplusplus,
+  SiR,
+  SiPostgresql,
+  SiSwift,
 } from 'react-icons/si';
-import {
-  FaPython,
-  FaJava,
-  FaJs,
-  FaNodeJs,
-  FaHtml5,
-  FaCss3Alt,
-} from 'react-icons/fa';
+import { FaJava } from 'react-icons/fa';
 
-const SkillIcon = ({ icon, label }) => (
-  <Box className="flex flex-col items-center justify-center p-4 rounded-lg hover:bg-gray-100 transition-colors duration-200">
-    <Icon as={icon} className="w-12 h-12 mb-2" />
-    <Text className="text-sm font-medium">{label}</Text>
-  </Box>
-);
+// const SkillIcon = ({ icon, label }) => (
+//   <Box className="flex flex-col items-center justify-center p-4 rounded-lg hover:bg-gray-100 transition-colors duration-200">
+//     <Icon as={icon} className="w-12 h-12 mb-2" />
+//     <Text className="text-sm font-medium">{label}</Text>
+//   </Box>
+// );
 
 const Skills = () => {
   const frameworks = [
     { icon: SiReact, label: 'React' },
     { icon: SiNextdotjs, label: 'Next.js' },
     { icon: SiTailwindcss, label: 'Tailwind CSS' },
+    { icon: SiAwsamplify, label: 'AWS Amplify' },
+    { icon: SiAngular, label: 'Angular' },
+    { icon: SiNodedotjs, label: 'NodeJS' },
+    { icon: SiExpress, label: 'Express' },
+    { icon: SiDocker, label: 'Docker' },
+    { icon: SiKubernetes, label: 'Kubernetes' },
   ];
 
   const languages = [
@@ -53,30 +62,21 @@ const Skills = () => {
     { icon: SiHtml5, label: 'HTML5' },
     { icon: SiCss3, label: 'CSS3' },
     { icon: SiMysql, label: 'MySQL' },
+    { icon: FaJava, label: 'Java' },
+    { icon: SiCplusplus, label: 'C++' },
+    { icon: SiR, label: 'R' },
+    { icon: SiPostgresql, label: 'PostgresSQL' },
+    { icon: SiSwift, label: 'Swift' },
   ];
 
   return (
     <Tabs variant="line" colorScheme="blue" width="100%">
       <TabList>
-        <Tab>Frameworks</Tab>
         <Tab>Languages</Tab>
+        <Tab>Frameworks</Tab>
       </TabList>
 
       <TabPanels>
-        {/* Frameworks Tab */}
-        <TabPanel>
-          <Flex alignItems="center" columnGap={4} marginTop={2} flexWrap="wrap">
-            {frameworks.map((framework, index) => (
-              <Tooltip
-                key={index}
-                label={framework.label}
-                aria-label={framework.label}
-              >
-                <Icon as={framework.icon} boxSize={8} />
-              </Tooltip>
-            ))}
-          </Flex>
-        </TabPanel>
         {/* <Flex alignItems="center" columnGap={4} marginTop={2}>
             {techStack.map((imageUrl, index) => (
               <Image  
@@ -101,6 +101,21 @@ const Skills = () => {
               <Icon key={index} as={language.icon} boxSize={8} />
             ))}
           </Box>
+        </TabPanel>
+
+        {/* Frameworks Tab */}
+        <TabPanel>
+          <Flex alignItems="center" columnGap={4} marginTop={2} flexWrap="wrap">
+            {frameworks.map((framework, index) => (
+              <Tooltip
+                key={index}
+                label={framework.label}
+                aria-label={framework.label}
+              >
+                <Icon as={framework.icon} boxSize={8} />
+              </Tooltip>
+            ))}
+          </Flex>
         </TabPanel>
       </TabPanels>
     </Tabs>
